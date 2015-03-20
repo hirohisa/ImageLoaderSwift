4b0f6b11dcd23b6bac979bd09a69730f820a34e4
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-03-21 04:03:08 +0900

    Use `String` when loading on Example project

--- ImageLoaderExample/NSURL.swift
+++ ImageLoaderExample/NSURL.swift
@@ -8,6 +8,21 @@
 
 import Foundation
 
+extension String {
+
+    static func imageURL(index: Int) -> String {
+
+        var number: NSString = index.description
+        while (number.length < 3) {
+            number = "0\(number)"
+        }
+        let string: String = "https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage\(number).jpg"
+
+        return string
+    }
+
+}
+
 extension NSURL {
 
     class func imageURL(index: Int) -> NSURL {
