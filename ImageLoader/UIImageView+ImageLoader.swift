774f38814015758961c3c274f66bbcf8c22abb89
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-11-19 18:46:21 +0900

    Bugfix: remove loader before calling success completion

--- ImageLoader/UIImageView+ImageLoader.swift
+++ ImageLoader/UIImageView+ImageLoader.swift
@@ -33,9 +33,9 @@ extension UIImageView {
         }
     }
 
-    private var block: AnyObject? {
+    private var block: Block? {
         get {
-            return objc_getAssociatedObject(self, &ImageLoaderBlockKey)
+            return objc_getAssociatedObject(self, &ImageLoaderBlockKey) as? Block
         }
         set(newValue) {
             objc_setAssociatedObject(self, &ImageLoaderBlockKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
@@ -56,28 +56,22 @@ extension UIImageView {
 
     public func cancelLoading() {
         if let URL = URL {
-            imageLoader.cancel(URL, block: block as? Block)
+            imageLoader.cancel(URL, block: block)
         }
     }
 
     // MARK: - private
-    private static let _requesting_queue = dispatch_queue_create("swift.imageloader.queues.requesting", DISPATCH_QUEUE_SERIAL)
+    private static let _Queue = dispatch_queue_create("swift.imageloader.queues.request", DISPATCH_QUEUE_SERIAL)
 
     private func _load(URL: NSURL, completionHandler: CompletionHandler?) {
 
         let closure: CompletionHandler = { URL, image, error, cacheType in
-
             dispatch_async(dispatch_get_main_queue()) { [weak self] in
-                // requesting is success then set image
-                if let wSelf = self, let thisURL = wSelf.URL, var image = image where thisURL.isEqual(URL) {
-                    if wSelf.imageLoader.automaticallyAdjustsSize {
-                        image = image.adjusts(wSelf.frame.size, scale: UIScreen.mainScreen().scale, contentMode: wSelf.contentMode)
-                    }
-
-                    wSelf.image = image
+                if let wSelf = self, thisURL = wSelf.URL, image = image where thisURL.isEqual(URL) {
+                    wSelf.imageLoader_setImage(image)
                 }
-                completionHandler?(URL, image, error, cacheType)
             }
+            completionHandler?(URL, image, error, cacheType)
         }
 
         // caching
@@ -86,16 +80,24 @@ extension UIImageView {
             return
         }
 
-        dispatch_async(UIImageView._requesting_queue) { [weak self] in
+        dispatch_async(UIImageView._Queue) { [weak self] in
             guard let wSelf = self else {
                 return
             }
 
             let block = Block(completionHandler: closure)
-            wSelf.imageLoader.load(URL).appendBlock(block)
+            let loader = wSelf.imageLoader.load(URL)
+                loader.appendBlock(block)
             wSelf.block = block
         }
+    }
 
+    private func imageLoader_setImage(image: UIImage) {
+        if imageLoader.automaticallyAdjustsSize {
+            self.image = image.adjusts(frame.size, scale: UIScreen.mainScreen().scale, contentMode: contentMode)
+        } else {
+            self.image = image
+        }
     }
 
 }
