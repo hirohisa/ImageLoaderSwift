d0b5ed928bf5b1375db9166a0dbd078139b595fb
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-12-01 20:44:59 +0900

    Move stubs logic to ImageLoaderTests

--- ImageLoaderTests/LoaderTests.swift
+++ ImageLoaderTests/LoaderTests.swift
@@ -9,73 +9,8 @@
 import UIKit
 import XCTest
 @testable import ImageLoader
-import OHHTTPStubs
 
-extension NSURLSessionTaskState {
-
-    func toString() -> String {
-        switch self {
-        case Running:
-            return "Running"
-        case Suspended:
-            return "Suspended"
-        case Canceling:
-            return "Canceling"
-        case Completed:
-            return "Completed"
-        }
-    }
-}
-
-extension State {
-
-    func toString() -> String {
-        switch self {
-        case .Ready:
-            return "Ready"
-        case Running:
-            return "Running"
-        case Suspended:
-            return "Suspended"
-        }
-    }
-}
-
-class LoaderTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-        setUpOHHTTPStubs()
-    }
-
-    override func tearDown() {
-        removeOHHTTPStubs()
-        super.tearDown()
-
-    }
-
-    func setUpOHHTTPStubs() {
-        OHHTTPStubs.stubRequestsPassingTest({ request -> Bool in
-            return true
-            }, withStubResponse: { request in
-                let data = try! NSJSONSerialization.dataWithJSONObject([:], options: [])
-                let response = OHHTTPStubsResponse(data: data, statusCode: 200, headers: nil)
-
-                if let path = request.URL?.path as String? {
-                    if let i = Int(path) where 400 <= i && i < 600 {
-                        response.statusCode = Int32(i)
-                    }
-                }
-
-                response.responseTime = 1
-
-                return response
-        })
-    }
-
-    func removeOHHTTPStubs() {
-        OHHTTPStubs.removeAllStubs()
-    }
+class LoaderTests: ImageLoaderTests {
 
     func testLoad() {
 
---
+++ ImageLoaderTests/ManagerTests.swift
@@ -0,0 +1,13 @@
+//
+//  ManagerTests.swift
+//  ImageLoader
+//
+//  Created by Hirohisa Kawasaki on 12/1/15.
+//  Copyright © 2015 Hirohisa Kawasaki. All rights reserved.
+//
+
+import XCTest
+@testable import ImageLoader
+
+class ManagerTests: ImageLoaderTests {
+}
