8c4e2627529769352ea9ee25d535adfd3ea6fd83
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2018-04-08 12:44:50 +0900

    Add Package.swift, Support SPM

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//  swift-tools-version:4.1
+//
+//  Package.swift
+//  ImageLoader
+//
+//  Created by Hirohisa Kawasaki on 4/8/18.
+//  Copyright © 2018 Hirohisa Kawasaki. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "ImageLoader",
+    swiftLanguageVersions: [4]
+)
